a8afe749aa97d23435fbe1930defa4815aea5c74
Boni Malunes <105722911+malunes0418@users.noreply.github.com>
2023-06-11 11:58:59 +0000

    added hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,44 +15,61 @@ const Navbar: React.FC = () => {
         <a href="#" className="logo font-bold text-white text-2xl">
           <span>OM</span>EGA
         </a>
-        <i
-          className="fas fa-bars text-white text-2xl cursor-pointer md:hidden"
+        <FontAwesomeIcon
+          icon={isOpen ? faTimes : faBars}
+          className="text-white text-2xl cursor-pointer md:hidden"
           onClick={toggleMenu}
-        ></i>
+        />
         <ul
           className={`${
             isOpen ? "right-0" : "right-full"
           } transition duration-500 bg-red-700 fixed top-0 w-64 h-screen py-10 md:static md:bg-transparent md:w-auto md:h-auto md:p-0 md:flex md:items-center md:space-x-8`}
         >
-          <li>
-            <a href="#header" className="text-white font-medium">
+          <li className="md:mr-4">
+            <a
+              href="#header"
+              className="text-white font-medium py-2 px-4 block hover:text-red-300 hover:border-b-2 border-red-300 transition-all"
+            >
               Home
             </a>
           </li>
-          <li>
-            <a href="#about" className="text-white font-medium">
+          <li className="md:mr-4">
+            <a
+              href="#about"
+              className="text-white font-medium py-2 px-4 block hover:text-red-300 hover:border-b-2 border-red-300 transition-all"
+            >
               About
             </a>
           </li>
-          <li>
-            <a href="#services" className="text-white font-medium">
+          <li className="md:mr-4">
+            <a
+              href="#services"
+              className="text-white font-medium py-2 px-4 block hover:text-red-300 hover:border-b-2 border-red-300 transition-all"
+            >
               Services
             </a>
           </li>
-          <li>
-            <a href="#portfolio" className="text-white font-medium">
+          <li className="md:mr-4">
+            <a
+              href="#portfolio"
+              className="text-white font-medium py-2 px-4 block hover:text-red-300 hover:border-b-2 border-red-300 transition-all"
+            >
               Projects
             </a>
           </li>
           <li>
-            <a href="#contact" className="text-white font-medium">
+            <a
+              href="#contact"
+              className="text-white font-medium py-2 px-4 block hover:text-red-300 hover:border-b-2 border-red-300 transition-all"
+            >
               Contact Me
             </a>
           </li>
-          <i
-            className="fas fa-times text-white text-2xl absolute top-4 right-4 cursor-pointer md:hidden"
+          <FontAwesomeIcon
+            icon={faTimes}
+            className="text-white text-2xl absolute top-4 right-4 cursor-pointer md:hidden"
             onClick={toggleMenu}
-          ></i>
+          />
         </ul>
       </div>
     </div>
